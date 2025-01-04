4d0a43de2c959a41570d71ca5c6a19cf03c58e2f
lequangphuong <phuonglq0102@gmail.com>
2025-01-04 10:58:02 +0700

    Initial

--- Services/gateway/src/app.module.ts
+++ Services/gateway/src/app.module.ts
@@ -11,25 +11,25 @@ import { LoggerModule } from 'logger/logger.module';
   imports: [ConfigModule.forRoot(),
     LoggerModule,
   // ThrottlerModule.forRoot({ throttlers: [{ limit: 10, ttl: 60 }] }),
-  // ClientsModule.register([
-  //   {
-  //     name: 'APP_GATEWAY',
-  //     transport: Transport.KAFKA,
-  //     options: {
-  //       client: {
-  //         clientId: 'app-gateway',
-  //         brokers: ['broker-1:19092'],
-  //       },
-  //       consumer: {
-  //         groupId: 'kafka-microservices',
-  //       },
-  //       producer: {
-  //         createPartitioner: Partitioners.LegacyPartitioner
-  //       }
-  //       // producerOnlyMode: true
-  //     },
-  //   },
-  // ]),
+  ClientsModule.register([
+    {
+      name: 'APP_GATEWAY',
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'app-gateway',
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'kafka-microservices',
+        },
+        producer: {
+          createPartitioner: Partitioners.LegacyPartitioner
+        }
+        // producerOnlyMode: true
+      },
+    },
+  ]),
   ],
   controllers: [AppController],
   providers: [AppService],
